001268d2cab71a83a94a2e2f7814468ed93fb5b4
Migs <cdtadifa21@gmail.com>
2025-12-30 22:57:21 +0800

    added login scenarios

--- tests/auth/login.spec.ts
+++ tests/auth/login.spec.ts
@@ -1,1 +1,32 @@
-// login
+import { test } from '@playwright/test';
+import { homePage } from '../../pages/home.page';
+
+test.describe('User Login', () => {
+  test('should login with valid credentials', async ({ page }) => {
+    // test code here
+  });
+
+  test('Login with invalid password', async ({ page }) => {
+    // test code here
+  });
+
+  test('Login with invalid email', async ({ page }) => {
+    // test code here
+  });
+
+  test('Login with empty email', async ({ page }) => {
+    // test code here
+  });
+
+  test('Login with empty password', async ({ page }) => {
+    // test code here
+  });
+
+  test('Login with both empty', async ({ page }) => {
+    // test code here
+  });
+
+  test('Login multiple times with invalid account', async ({ page }) => {
+    // test code here
+  });
+});
