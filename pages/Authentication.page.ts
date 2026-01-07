37f09609a8f8c820db50f06d13a6660530e0c88a
Migs <cdtadifa21@gmail.com>
2026-01-07 18:20:32 +0800

    fix variable name for login

--- pages/Authentication.page.ts
+++ pages/Authentication.page.ts
@@ -3,10 +3,14 @@ import { Page, Locator, expect } from '@playwright/test';
 export class AuthenticationPage {
 
 //Locators class properties 
-  private readonly eAddress: Locator;
-  private readonly password: Locator;
-  private readonly btnLogin: Locator; 
-  private readonly loginerror: Locator;
+  private readonly logineAddress: Locator;
+  private readonly loginPassword: Locator;
+  private readonly loginBtnLogin: Locator; 
+  private readonly loginError: Locator;
+  private readonly nameField: Locator;
+  private readonly emailField: Locator ;
+  private readonly btnSignup: Locator;
+  private readonly registrationError: Locator;
 
 
 // constructor
@@ -15,41 +19,47 @@ export class AuthenticationPage {
 
     //Sign In Locators
     const loginForm = page.locator('form', { hasText: 'Login' });
-    this.eAddress = loginForm.getByRole('textbox', { name: 'Email Address' });
-    this.password = loginForm.getByRole('textbox', { name: 'Password' });
-    this.btnLogin = page.getByRole('button', { name: 'Login' });
-    this.loginerror = loginForm.getByText('Your email or password is incorrect!');
+    this.logineAddress = loginForm.getByRole('textbox', { name: 'Email Address' });
+    this.loginPassword = loginForm.getByRole('textbox', { name: 'Password' });
+    this.loginBtnLogin = page.getByRole('button', { name: 'Login' });
+    this.loginError = loginForm.getByText('Your email or password is incorrect!');
     
+    //Register Locators
+    const regForm = page.locator('form', { hasText: 'signup' });
+    this.nameField = regForm.getByRole('textbox', { name: 'Name' });
+    this.emailField = regForm.getByRole('textbox', { name: 'Email Address' });
+    this.btnSignup = page.getByRole('button', { name: 'Signup' });
+    this.registrationError = regForm.getByText('Email Address already exist!');
   }
 
 
 //function Methods
   async enterUsernameField(userName: string): Promise<void> {
-    await this.eAddress.fill(userName); 
+    await this.logineAddress.fill(userName); 
   }
 
   async enterPasswordField(password: string): Promise<void> {
-    await this.password.fill(password); 
+    await this.loginPassword.fill(password); 
   }
 
   async clickLoginButton(): Promise<void> {
-    await this.btnLogin.click();
+    await this.loginBtnLogin.click();
   }
 
 //assertion Methods
   async expectLoginErrorVisible(): Promise<void> {
-    await expect(this.loginerror).toBeVisible();
-    await expect(this.loginerror).toHaveText('Your email or password is incorrect!');
+    await expect(this.loginError).toBeVisible();
+    await expect(this.loginError).toHaveText('Your email or password is incorrect!');
   }
 
   async expectLoginError(errorMessage: string): Promise<void> {
-    const validationMessage = await this.eAddress.evaluate((el: HTMLInputElement) => el.validationMessage);
+    const validationMessage = await this.logineAddress.evaluate((el: HTMLInputElement) => el.validationMessage);
     if (errorMessage === "Invalid email") {
       expect(validationMessage).toContain("Please include an '@' in the email address.");
     } else if (errorMessage === "Empty email") {
       expect(validationMessage).toContain("Please fill out this field.");
     } else if (errorMessage === "Empty password"){
-      const validationMessage = await this.password.evaluate((el: HTMLInputElement) => el.validationMessage);
+      const validationMessage = await this.loginPassword.evaluate((el: HTMLInputElement) => el.validationMessage);
       expect(validationMessage).toContain("Please fill out this field.");
     }
 
