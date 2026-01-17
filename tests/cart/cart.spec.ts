112acaaa27a5438435762163da00fb90f1e8594b
Migs <cdtadifa21@gmail.com>
2026-01-17 20:52:27 +0800

    added base

--- tests/cart/cart.spec.ts
+++ tests/cart/cart.spec.ts
@@ -1,1 +1,25 @@
-// carts
+import { test, expect } from '../../utils/base';
+
+import userData from '../../fixtures/userData.json';
+
+import { dataGenerator } from '../../utils/testData';
+
+
+test.describe('Cart Scenario', () => {
+
+//   const user =  dataGenerator.fullName();
+//   const email = dataGenerator.validEmail();
+//   const bday = dataGenerator.randomBirthday();
+//   const randomCountry = Math.floor(Math.random() * 7);
+
+    test.beforeEach(async ({ authLogin }) => {
+       
+      });
+
+
+      test('Successfully added item to the cart', async ({page}) => {
+        // Test steps to add item to cart
+        // await authpage.registerNewUser(user, email, bday, userData.countries[randomCountry], userData.validPassword);
+        // await authpage.expectAccountCreated();
+      });
+});
